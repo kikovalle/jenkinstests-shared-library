2c36e5bc8d464025998d1a7c3a400273aea9b3a1
kikovalle <kikovalle@gmail.com>
2022-02-17 10:46:15 +0100

    Initial commit of example lib to use from pipelinetests jenkinsfile

---
+++ src/com/github/kikovalle/jenkins/GlobalVars.groovy
@@ -0,0 +1,8 @@
+package com.girhub.kikovalle.jenkins
+
+class GlobalVars {
+
+  static String defaultDeveloper = "Kiko Valle González"
+  static String githubUrl = "https://github.com/kikovalle/jenkinstests-shared-library"
+
+}
---
+++ vars/sayHello.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+
+def greet(String name = "unknown") {
+  echo "Greated to greet you Mr ${name}"
+}
+

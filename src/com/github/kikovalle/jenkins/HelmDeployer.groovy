6878ec649ac442240ab1f6ca08f1e17b7473bbbb
Francisco Valle González <kikovalle@gmail.com>
2022-02-18 10:58:36 +0100

    Update HelmDeployer.groovy
    Removed calls that are included in agent container image

--- src/com/github/kikovalle/jenkins/HelmDeployer.groovy
+++ src/com/github/kikovalle/jenkins/HelmDeployer.groovy
@@ -3,16 +3,6 @@ package com.github.kikovalle.jenkins
 class HelmDeployer {
   
   static def deploy(script, appNameAndFolder, releaseNamespace) {
-    script.sh "echo 'Example to test deployment with helm from a pipeline using a global shared library for app ${appNameAndFolder} and release ${releaseNamespace}'"
-    script.sh "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.4/bin/linux/amd64/kubectl"
-    script.sh "chmod +x ./kubectl"
-    script.sh "mv ./kubectl /usr/local/bin/kubectl"
-    
-    script.sh "echo 'Going to install helm tool in this step!'"
-    script.sh "wget https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz"
-    script.sh "tar zxfv helm-v3.2.4-linux-amd64.tar.gz"
-    script.sh "cp linux-amd64/helm ."
-    
     script.sh "echo 'Launching helm to upgrade ${appNameAndFolder} in ${releaseNamespace}!'"
     script.sh "./helm upgrade --install --wait ${appNameAndFolder} ./${appNameAndFolder} -n ${releaseNamespace}"
   }

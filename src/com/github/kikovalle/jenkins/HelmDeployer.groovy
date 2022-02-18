4b34e88804a3be8aee309255e9c3c6bef3466fde
Francisco Valle González <kikovalle@gmail.com>
2022-02-18 14:11:26 +0100

    Update HelmDeployer.groovy
    Removed --name option to fit helm version requirements

--- src/com/github/kikovalle/jenkins/HelmDeployer.groovy
+++ src/com/github/kikovalle/jenkins/HelmDeployer.groovy
@@ -4,7 +4,7 @@ class HelmDeployer {
   
   static def deploy(script, releaseName, appNameAndFolder, releaseNamespace) {
     script.sh "echo 'Launching helm to upgrade ${appNameAndFolder} in ${releaseNamespace}!'"
-    script.sh "helm upgrade --install --wait --name ${releaseName} ./${appNameAndFolder} -n ${releaseNamespace}"
+    script.sh "helm upgrade --install --wait  ${releaseName} ./${appNameAndFolder} -n ${releaseNamespace}"
   }
 
 }

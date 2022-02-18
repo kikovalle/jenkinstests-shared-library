8c82d5ba709058c8a422e0b368043797adc1e993
Francisco Valle González <kikovalle@gmail.com>
2022-02-18 13:50:51 +0100

    Update HelmDeployer.groovy
    Added --wait option to helm call

--- src/com/github/kikovalle/jenkins/HelmDeployer.groovy
+++ src/com/github/kikovalle/jenkins/HelmDeployer.groovy
@@ -4,7 +4,7 @@ class HelmDeployer {
   
   static def deploy(script, appNameAndFolder, releaseNamespace) {
     script.sh "echo 'Launching helm to upgrade ${appNameAndFolder} in ${releaseNamespace}!'"
-    script.sh "helm upgrade --install ${appNameAndFolder} ./${appNameAndFolder} -n ${releaseNamespace}"
+    script.sh "helm upgrade --install --wait ${appNameAndFolder} ./${appNameAndFolder} -n ${releaseNamespace}"
   }
 
 }

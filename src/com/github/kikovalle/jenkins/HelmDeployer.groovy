af7d707d7c89c273636a6f7eb6ae07fb1c89b91e
Francisco Valle González <kikovalle@gmail.com>
2022-02-21 08:58:01 +0100

    Update HelmDeployer.groovy
    Added Serializable annotation to allow saving state.

--- src/com/github/kikovalle/jenkins/HelmDeployer.groovy
+++ src/com/github/kikovalle/jenkins/HelmDeployer.groovy
@@ -1,6 +1,6 @@
 package com.github.kikovalle.jenkins
 
-class HelmDeployer {
+class HelmDeployer implements Serializable {
   
   static def deploy(script, releaseName, appNameAndFolder, releaseNamespace) {
     script.sh "echo 'Launching helm to upgrade ${appNameAndFolder} in ${releaseNamespace}!'"

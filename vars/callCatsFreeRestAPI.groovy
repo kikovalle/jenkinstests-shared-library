37bd9a6ce0e7a0a2dc08a4b9758268a2fc6f0b01
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 12:42:19 +0100

    Update callCatsFreeRestAPI.groovy
    Increased number of characters in condition

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -2,10 +2,10 @@
 def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
   def response = sh(returnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.fact'").trim()
-  if (response && response.length() > 50) {
+  if (response && response.length() > 150) {
     println "Result       : ${response}"
   } else {
-    println "Result       : ${response} is shorter than 50 characters, so this build will be marked as unstable"
-    error "Response is shorter than 50 characters"
+    println "Result       : ${response} is shorter than 150 characters, so this build will be marked as unstable"
+    error "Response is shorter than 150 characters"
   }
 }

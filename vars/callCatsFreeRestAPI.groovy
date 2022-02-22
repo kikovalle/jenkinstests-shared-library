091bb2188ae7fb49f61c365924cab06633954438
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 16:48:54 +0100

    Removed option of jq

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -3,10 +3,10 @@
 def call(String name = "unknown") {
   def minLength = 200
   def srvUrl = "https://catfact.ninja/fact"
-  def preResponse = sh(returnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output -c '.' ")
+  def preResponse = sh(returnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.' ")
   println "Processed response: ${preResponse}"
-  def fact = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output -c '.fact' ").trim()
-  def length  = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output -c '.length' ").trim()
+  def fact = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output '.fact' ").trim()
+  def length  = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output '.length' ").trim()
   println "Fact in response       : ${fact}"
   println "Length of fact text    : ${length}"
   if (Integer.parseInt(length) > minLength) {

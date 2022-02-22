2fed8ea3f59776e1f8913eb4eae9dcf7a49d4f83
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 15:52:11 +0100

    Changed jq options

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
-  def srvResponse = sh(resturnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.' ")
+  def srvResponse = sh(resturnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq -c '.' ")
   println "Response before processing it: ${srvResponse}"
-  def response = sh(returnStdout: true, script: "echo ${srvResponse} | jq --raw-output '.fact'").trim()
+  def response = sh(returnStdout: true, script: "echo ${srvResponse} | jq -c '.fact'").trim()
   println "Processed response: ${response}"
   if (response && response.length() > 300) {
     println "Result       : ${response}"

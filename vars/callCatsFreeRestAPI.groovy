b49f58891588823b2c66f078576dcb4cca92f517
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 12:12:19 +0100

    Changed sh call to tell we want to get output
    Changed sh call to tell we want to get output message and store it into a variable

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
-  println "Result       : ${sh "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.fact' "}"
+  def response = sh(returnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.fact'").trim()
+  println "Result       : ${response}"
 }

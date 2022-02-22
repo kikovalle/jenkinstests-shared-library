58ed0c6811a39c8b0eddca1451d14f73d2bed0ae
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 16:00:54 +0100

    Split calls

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
-  def response = sh(resturnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output -c '.fact' ")
+  def preResponse = sh(returnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output -c '.' ")
+  println "Processed response: ${preResponse}"
+  def response = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output -c '.fact' ")
   println "Processed response: ${response}"
   if (response && response.length() > 300) {
     println "Result       : ${response}"

7f589edde6ac7af3841a44bdc39b6899fe3858e0
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 12:05:01 +0100

    Changes
    Lets see if this works

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy
 def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
-  def result = sh "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.fact'"
-  println "Result       : ${result}"
+  println "Result       : ${sh "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.fact' | echo"}"
 }

e0b8f28ee6a8017d28fcecbc8a55cc927af2a6fd
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 12:44:15 +0100

    Update callCatsFreeRestAPI.groovy
    Length increased to 300

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -2,10 +2,10 @@
 def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
   def response = sh(returnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.fact'").trim()
-  if (response && response.length() > 150) {
+  if (response && response.length() > 300) {
     println "Result       : ${response}"
   } else {
-    println "Result       : ${response} is shorter than 150 characters, so this build will be marked as unstable"
-    error "Response is shorter than 150 characters"
+    println "Result       : ${response} is shorter than 300 characters, so this build will be marked as unstable"
+    error "Response is shorter than 300 characters"
   }
 }

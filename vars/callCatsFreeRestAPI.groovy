7e17660975fa89a18efd1872a04c0fff24f3e1f9
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 16:16:57 +0100

    Some changes
    Take the length from the json response.

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -1,13 +1,17 @@
 #!/usr/bin/env groovy
+
 def call(String name = "unknown") {
+  def minLength = 200
   def srvUrl = "https://catfact.ninja/fact"
   def preResponse = sh(returnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output -c '.' ")
   println "Processed response: ${preResponse}"
-  def response = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output -c '.fact' ")
-  println "Processed response: ${response}"
-  if (response && response.length() > 300) {
+  def fact = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output -c '.fact' ")
+  def length  = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output -c '.length' ")
+  println "Fact in response       : ${fact}"
+  println "Length of fact text    : ${length}"
+  if (length > minLength) {
     println "Result       : ${response}"
   } else {
-    unstable("Result length is not long enough (300 characters minimun)!!!")
+    unstable("Result length (${length}) is not long enough (${minLength} characters minimun)!!!")
   }
 }

b3798976edaa20ab92164a73592f6d26e3790c4a
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 16:22:12 +0100

    Trim responses

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -5,8 +5,8 @@ def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
   def preResponse = sh(returnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output -c '.' ")
   println "Processed response: ${preResponse}"
-  def fact = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output -c '.fact' ")
-  def length  = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output -c '.length' ")
+  def fact = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output -c '.fact' ").trim()
+  def length  = sh(returnStdout: true, script: "echo '${preResponse}' | jq --raw-output -c '.length' ").trim()
   println "Fact in response       : ${fact}"
   println "Length of fact text    : ${length}"
   if (Integer.parseInt(length) > minLength) {

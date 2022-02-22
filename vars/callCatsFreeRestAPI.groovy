21f72d026f99ef909281682fcce6c2ca3c705559
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 15:38:32 +0100

    Split sh calls in two
    Seems to be a better way to do this as its possible now to do searches with full response variable without calling service again and again.

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -1,7 +1,10 @@
 #!/usr/bin/env groovy
 def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
-  def response = sh(returnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.fact'").trim()
+  def srvResponse = sh(resturnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl}")
+  println "Response before processing it: ${srvResponse}"
+  def response = sh(returnStdout: true, script: "echo ${srvResponse} | jq --raw-output '.fact'").trim()
+  println "Processed response: ${response}"
   if (response && response.length() > 300) {
     println "Result       : ${response}"
   } else {

302dde9c4febb208a0b4fe0fac643ef261dd9a8f
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 12:06:21 +0100

    Change

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
-  println "Result       : ${sh "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.fact' | echo"}"
+  println "Result       : ${sh "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.fact' "}"
 }

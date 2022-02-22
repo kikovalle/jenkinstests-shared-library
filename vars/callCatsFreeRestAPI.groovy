2006bcfe6f957b5a10cb2508f3ef1b5e21f3a2ca
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 15:40:42 +0100

    Changes

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
-  def srvResponse = sh(resturnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl}")
+  def srvResponse = sh(resturnStdout: true, script: "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.' ")
   println "Response before processing it: ${srvResponse}"
   def response = sh(returnStdout: true, script: "echo ${srvResponse} | jq --raw-output '.fact'").trim()
   println "Processed response: ${response}"

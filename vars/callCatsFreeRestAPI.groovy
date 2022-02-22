263bcbff180ebcf2b0f0864fc10a55ce8af34211
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 11:40:37 +0100

    Changed to use curl call
    Changed to use curl call

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -1,22 +1,5 @@
 #!/usr/bin/env groovy
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
-
-import groovyx.net.http.RESTClient
-
 def call(String name = "unknown") {
-  def srvUrl = "https://catfact.ninja"
-  
-  def response
-  try {
-    def client = new RESTClient(srvUrl)
-    response = client.get(path: "/fact")
-    println "Full response: ${response}"
-    println "Status       : ${response.status}"
-    println "Body         : ${response.data.text}"
-    echo "Response OK"
-    echo "${response}"
-  } catch (Exception e) {
-    println "Error      : ${e.statusCode}"
-    println "Message    : ${e.response.data}"
-  }
+  def srvUrl = "https://catfact.ninja/fact"
+  sh "curl -X GET -H 'content-type: application/json' ${srvUrl}"
 }

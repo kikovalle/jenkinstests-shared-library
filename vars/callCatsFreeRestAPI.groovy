3c3b7ee6c7fb627b92596cb3ae4822fb6005c2a9
Francisco Valle González <kikovalle@gmail.com>
2022-02-22 11:52:10 +0100

    jq to parse response
    Using another tool to parse json response (maybe change in container needed)

--- vars/callCatsFreeRestAPI.groovy
+++ vars/callCatsFreeRestAPI.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 def call(String name = "unknown") {
   def srvUrl = "https://catfact.ninja/fact"
-  sh "curl -X GET -H 'content-type: application/json' ${srvUrl}"
+  def result = sh "curl -X GET -H 'content-type: application/json' ${srvUrl} | jq --raw-output '.fact'"
+  println "Result       : ${result}"
 }
